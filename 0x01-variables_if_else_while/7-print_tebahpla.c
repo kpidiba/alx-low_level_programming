6295e62153fe5ca59a07846042ec445136f8ac8e
dprogrammer <kpidibadavid1@gmail.com>
2022-09-11 16:35:07 +0000

    tebahphla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always success
+ */
+
+int main(void)
+{
+	int z;
+
+	for (z = 'z' ; z >= 'a'; z--)
+		putchar(z);
+	putchar('\n');
+return (0);
+
+}
