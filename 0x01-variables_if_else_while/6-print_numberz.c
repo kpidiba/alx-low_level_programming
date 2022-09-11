09fa6dcd163888b47930c87ba315fcf2be9292c0
dprogrammer <kpidibadavid1@gmail.com>
2022-09-11 16:33:14 +0000

    number z

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always success
+ */
+
+int main(void)
+{
+	int z;
+
+	for (z = '0' ; z <= '9'; z++)
+		putchar(z);
+	putchar('\n');
+return (0);
+
+}
