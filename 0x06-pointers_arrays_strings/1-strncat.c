ed6f36a6cbe82b94902ccc0a5aacbf993712f033
David Kpidiba <kpidibadavid1@gmail.com>
2022-09-21 17:53:52 +0000

    leet

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,31 +1,16 @@
 #include "main.h"
-
+#include <string.h>
 /**
- * _strncat - concatenates specified values from src to dest
+ * _strncat -> concatenates specified values from src to dest
  * @src: second string to copy from
  * @dest: string to be overwritten
- * @n: number of values to concatenate
+ * @n: 3rd param
  * Description: concatenates n number of values from src to end of dest
- * Return: pointer to dest
+ * Return: string
  **/
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int i, j;
-
-	i = 0;
-
-	while (dest[i] != '\0')
-		i++;
-
-	for (j = 0; j < n; j++)
-	{
-		dest[i] = src[j];
-		i++;
-	}
-
-	if (dest[i - 1] != '\0')
-		dest[i] = '\0';
-
+strncat(dest, src, n);
 	return (dest);
 }
